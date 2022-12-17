afaf65bb98dbe5f0034b44a9bc76ca692b1a4995
Poyo <poyoarya@gmail.com>
2022-12-17 14:20:07 -0800

    Add tooltips

--- src/components/Character.svelte
+++ src/components/Character.svelte
@@ -9,6 +9,8 @@
     export let proportion: number;
     
     $: dashArray = proportion * circumference;
+
+    $: percentage = (proportion * 100).toFixed(1) + "%";
 </script>
 
 <div class="container">
@@ -17,6 +19,7 @@
         <circle stroke="#2ECC40" stroke-width=4 fill="transparent" r=18 cx=22 cy=22 stroke-dasharray={`${dashArray} ${circumference - dashArray}`} stroke-dashoffset={circumference / 4}></circle>
     </svg>
     <img src="/characters/{getSlugFromCharacter(character)}.png" alt={character}>
+    <div class="tooltip">{character} ({percentage})</div>
 </div>
 <style>
     .container {
@@ -31,5 +34,29 @@
         width: 22px;
         top: 11px;
         left: 11px;
+    }
+
+    .tooltip {
+        white-space: nowrap;
+        opacity: 0;
+        transition-property: opacity;
+        transition-duration: 100ms;
+        pointer-events: none;
+        font-family: "Maven Pro";
+        font-size: 14px;
+        line-height: 24px;
+        position: absolute;
+        z-index: 1;
+        background-color: var(--color-background-lightest);
+        height: 24px;
+        margin: 0 auto;
+        padding: 2px 9px;
+        border-radius: 4px;
+        transform: translateX(calc(-50% + 22px));
+        user-select: none;
+    }
+
+    .container:hover > .tooltip {
+        opacity: 1;
     }
 </style>
